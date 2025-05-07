e66e282229ae209c14fb8d18c9caca7e1f768f13
Rick03098 <18811600598@qq.com>
2025-05-06 20:56:34 -0500

    chore: 全局挂载fetch拦截日志，定位/api/parseJDFile请求来源，便于排查异常请求

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import '../styles/globals.css';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const origFetch = window.fetch;
+      window.fetch = function(...args) {
+        if (typeof args[0] === 'string' && args[0].includes('/api/parseJDFile')) {
+          console.warn('全局拦截到 fetch /api/parseJDFile:', args, new Error('调用栈').stack);
+        }
+        return origFetch.apply(this, args);
+      };
+    }
+  }, []);
   return <Component {...pageProps} />;
 }
 
